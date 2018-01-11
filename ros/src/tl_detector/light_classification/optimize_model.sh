617aa3288abf45a181aecbac4e991f00fd0f0512
Benjamin Söllner <benjamin.soellner@gmail.com>
2018-01-11 18:40:24 +0100

    bugfix: optimize model works now

--- ros/src/tl_detector/light_classification/optimize_model.sh
+++ ros/src/tl_detector/light_classification/optimize_model.sh
@@ -14,7 +14,6 @@ TF_MODEL_TENSOR_OUT="detection_classes,detection_scores"
 
 cd $TF_SRC_DIR
 
-# TODO FIXME throws error if finished model is put into graph
 bazel build tensorflow/tools/graph_transforms:transform_graph
 bazel-bin/tensorflow/tools/graph_transforms/transform_graph \
 --in_graph=$TF_MODEL_ORIGINAL \
@@ -24,12 +23,9 @@ bazel-bin/tensorflow/tools/graph_transforms/transform_graph \
 --transforms='
   add_default_attributes
   strip_unused_nodes(type=float, shape="1,299,299,3")
-  remove_nodes(op=Identity, op=CheckNumerics)
   fold_constants(ignore_errors=true)
   fold_batch_norms
   fold_old_batch_norms
   fuse_resize_and_conv
-  quantize_weights
-  quantize_nodes
   strip_unused_nodes
   sort_by_execution_order'
