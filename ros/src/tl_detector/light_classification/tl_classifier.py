250d241d5753c118fc0755cfad3f355411ab6883
Benjamin Söllner <benjaminsoellner@users.noreply.github.com>
2018-01-14 15:46:19 +0100

    bugfix: tl_classifier & tl_detector, refactored: model test scripts (#5)
    bugfix: in tl_classifier & tl_detector, refactored: moved model test scripts to "models" directory

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -7,30 +7,28 @@ DETECTION_LIMIT = 0.5
 
 class TLClassifier(object):
     def __init__(self):
-        """
         rospy.logdebug("TLClassifier::__init__ started.")
 
         # Location of pre-trained inference model
-        model_path = './light_classification/model/frozen_inference_graph.pb' 
+        model_path = './light_classification/model/frozen_inference_graph.pb'
         self.inference_graph = tf.Graph()
 
         with self.inference_graph.as_default():
             current_graph_def = tf.GraphDef()
-            
+
             # Load graph from file and imports it into the default graph
             with tf.gfile.GFile(model_path, 'rb') as file:
                 file_graph = file.read()
                 current_graph_def.ParseFromString(file_graph)
                 tf.import_graph_def(current_graph_def, name='')
-            
+
             self.image_tensor = self.inference_graph.get_tensor_by_name('image_tensor:0')
             self.d_boxes = self.inference_graph.get_tensor_by_name('detection_boxes:0')
             self.d_scores = self.inference_graph.get_tensor_by_name('detection_scores:0')
             self.d_classes = self.inference_graph.get_tensor_by_name('detection_classes:0')
             self.num_d = self.inference_graph.get_tensor_by_name('num_detections:0')
-        
+
         self.sess = tf.Session(graph = self.inference_graph)
-        """
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -44,11 +42,13 @@ class TLClassifier(object):
         # Runs inference on one image on the loaded graph
         with self.inference_graph.as_default():
             # Image is expanded to 4 dims - 1st dim batch size (=1)
-            image_4d = np.expand_dims(img, axis=0)
+            image_4d = np.expand_dims(image, axis=0)
             (boxes, scores, classes, num_det) = self.sess.run([self.d_boxes, self.d_scores, self.d_classes, self.num_d], feed_dict = {self.image_tensor: image_4d})
-        
+
         # Inference returns a (fixed) total of self.num_d detections - even those with low probabilities
-        r_boxes, r_scores, r_classes = []
+        r_boxes = []
+        r_scores = []
+        r_classes = []
         idx = 0
 
         # If the highest score is below detection probability, there is no traffic light visible or not clear enough, return unknown
@@ -67,7 +67,7 @@ class TLClassifier(object):
         yellow = [6]
         green = [7, 8, 9, 10, 11, 12]
         yellow_or_not = False
-        
+
         for classification in r_classes:
             if classification in red:
                 return TrafficLight.RED
@@ -77,4 +77,3 @@ class TLClassifier(object):
             return TrafficLight.YELLOW
         else:
             return TrafficLight.GREEN
-
