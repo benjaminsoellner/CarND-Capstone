00ead4ceac708f67f0dbd8df848f8921c6803122
Ubuntu <student@herbie.gycdv0rmabvublci4rqsmn0spa.ax.internal.cloudapp.net>
2018-01-21 13:42:32 +0000

    modified clone_user script for azure VM

--- ide_profiles/azure-vm/clone_user.sh
+++ ide_profiles/azure-vm/clone_user.sh
@@ -7,6 +7,6 @@ DEST=$2
 
 SRC_GROUPS=$(id -Gn ${SRC} | sed "s/${SRC} //g" | sed "s/ ${SRC}//g" | sed "s/ /,/g")
 SRC_SHELL=$(awk -F : -v name=${SRC} '(name == $1) { print $7 }' /etc/passwd)
-
 useradd --groups ${SRC_GROUPS} --shell ${SRC_SHELL} --create-home ${DEST}
+echo "source /opt/ros/kinetic/setup.bash" >> /home/${DEST}/.bashrc
 passwd ${DEST}
